0eac0b22cf490cdbd1ac757f6981022a798e903a
Oldemar Rodriguez <oldemar.rodriguez@ucr.ac.cr>
2015-11-04 01:10:28 +0000

    version 1.3

--- R/process.continuum.variable.R
+++ R/process.continuum.variable.R
@@ -1,5 +1,5 @@
 process.continuum.variable <-
 function(variableName, conceptColumns) {
   varType <- "$C"
-  return (fn$sqldf("SELECT '$varType', round(AVG($variableName), 2) AS $variableName FROM main.dataTable GROUP BY $conceptColumns ORDER BY $conceptColumns"))
+  return (sqldf(paste0("SELECT '", varType, "', round(AVG(", variableName, "), 2) AS ", variableName, " FROM main.dataTable GROUP BY ", conceptColumns, " ORDER BY ", conceptColumns)))
 }
--- R/process.interval.variable.R
+++ R/process.interval.variable.R
@@ -1,5 +1,5 @@
 process.interval.variable <-
-function(variableName, conceptColumns) {
-  varType <- "$I"
-  return (fn$sqldf("SELECT '$varType', MIN($variableName) AS $variableName, MAX($variableName) AS $variableName FROM main.dataTable GROUP BY $conceptColumns ORDER BY $conceptColumns"))
-}
+  function(variableName, conceptColumns) {
+    varType <- "$I"
+    return (sqldf(paste0("SELECT '", varType, "', MIN(", variableName, ") AS ", variableName, ", MAX(", variableName, ") AS ", variableName, " FROM main.dataTable GROUP BY ", conceptColumns, " ORDER BY ", conceptColumns)))
+  }
